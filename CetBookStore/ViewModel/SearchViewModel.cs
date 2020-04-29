878d9956ed1b379fb8a3805079dfb2dd59f98f5d
nazkorkmaz <nazkorkmaz07@hotmail.com>
2020-04-29 23:37:33 +0300

    Add project files.

---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+         [Display(Name ="Arama Metni")]
+        public string SearchText { get; set; }
+        [Display(Name ="Açıklamalarda Ara")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name ="Kategori Seçimi")]
+        public int? CategoryId { get; set; }
+
+        public List<Models.Book> Results { get; set; }
+
+        public int? minfiyat { get; set; }
+        public int? maxfiyat { get; set; }
+    }
+}
